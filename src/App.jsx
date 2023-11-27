af23df46039ce1d9a3e4c6367b0da23236dc1c52
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-11-27 18:42:18 -0300

    Create the pokeApi and pokemon-model services

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css"
+import pokeApi from "./services/pokeApi"
 import PokeCard from "./components/PokeCard";
+import { useState,useEffect } from "react";
 
 function App() {
+  const [pokemons, setPokemons] = useState([])
+  const [offset, setOffset] = useState(0)
+
+  useEffect(() => {
+    pokeApi.getPokemons(offset)
+      .then(pokemonDetails => setPokemons(pokemonDetails))
+  }, [offset])
+
+  function handleClick() {
+    setOffset((oldOffset => oldOffset + 12))
+  }
 
   return (
-    <section>
+    <section className="content">
       <h1>Pokédex</h1>
-      <ol className="poke-list">
-        <PokeCard />
+      <ol id="pokemon-list" className="pokemons">
+        {pokemons.map((pokemon) => <PokeCard key={pokemon.number} {...pokemon} />)}
+        <button onClick={handleClick}>Previous</button>
+        <button onClick={handleClick}>Next</button>
       </ol>
     </section>
   )
--- src/components/PokeCard/index.jsx
+++ src/components/PokeCard/index.jsx
@@ -1,13 +1,21 @@
 import styles from "./PokeCard.module.css"
 
-function PokeCard({}) {
+function PokeCard({ type, name, number, types, photo, }) {
+    let formattedNumber = number.toString();
+    while (formattedNumber.length < 3) formattedNumber = "0" + formattedNumber
+
     return (
-        <div className={styles.card_container}>
-            <img src="" alt="" />
-            <h2>Bulbasaur</h2>
-            <span>grass</span>
-            <span>earth</span>
-        </div>
+        <li className={`${styles.pokemon} ${type}`} id={`${type}`}>
+            <span className={styles.number}>#{formattedNumber}</span>
+            <span className={styles.name}>{name}</span>
+
+            <div className={styles.detail}>
+                <ol className={styles.types}>
+                    {types.map(type => <li className={`${styles.type} ${type}`}>{type}</li>)}
+                </ol>
+                <img className={styles.image} src={photo} alt={`Imagem de ${name}`} />
+            </div>
+        </li >
     )
 }
 
